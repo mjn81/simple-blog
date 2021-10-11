6ab6372e65649f24eae85024047d9d731770fb6c
mjn81 <mjjnn81@gmail.com>
2021-10-11 23:39:21 +0330

    topBar Compelete

--- src/template/TopBar.js
+++ src/template/TopBar.js
@@ -1,38 +1,41 @@
+import useFetch from "./useFetch";
+
+
+
+
 const TopBar = () => {
+    const res= useFetch('http://localhost:2020/topbar');
     return (  
         <div>
-            <header class="tmp-header">
-                <h1 class="title">
-                    <span class="Blue-sq">B</span>eep<span class="Red-dt">.</span>
+            <header className="tmp-header">
+                <h1 className="title">
+                    <span className="Blue-sq">B</span>eep<span className="Red-dt">.</span>
                 </h1>
-                <nav class="top-navbar">
+                <nav className="top-navbar">
                     <ul>
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                        <li><a href="#">FAQs</a></li>
+                    {
+                        res&& res.list.map(item=>(
+                            <li><a href={item.link}>{item.name}</a></li>
+                        ))
+                    }
                     </ul>
                 </nav>
-                <div class="nav-seprator">
+                <div className="nav-seprator">
 
                 </div>
-                <div class="tmp-side-nav">
-                    <button class="btn btn-rev">Log in</button>
-                    <button class="btn">Sign Up</button>
+                <div className="tmp-side-nav">
+                    <a className="btn btn-rev" href="/Login">Log in</a>
+                    <a className="btn" href="/SignUp">Sign Up</a>
                 </div>  
             </header>  
 
-            <section class="tmp-category">
-                    <h1 class="section-title">Blog</h1>
-                    <nav class="second-navbar">
+            <section className="tmp-category">
+                    <h1 className="section-title">Blog</h1>
+                    <nav className="second-navbar">
                         <ul>
-                            <li><a href="index.html " class="active">All</a></li>
-                            <li><a href="#">Featured</a></li>
-                            <li><a href="#">Popular</a></li>
-                            <li><a href="category.html">World</a></li>
-                            <li><a href="#">Bussiness</a></li>
-                            <li><a href="#">Technology</a></li>
-                            <li><a href="#">Safety</a></li>
+                          {res&&res.category.map(item=>(
+                              <li><a href={item.link}>{item.name}</a></li>
+                          ))}
                             <li><input type="search" id="search-bar" placeholder="search" /></li>
                         </ul>
                     </nav>
@@ -40,5 +43,5 @@ const TopBar = () => {
         </div>
     );
 }
- 
+
 export default TopBar;
